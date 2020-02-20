3c1d1c8b67323dabee68c39988b4af6e10382e73
Andrey Gluhih <andreygluhih99@gmail.com>
2020-02-20 16:44:13 +0300

    done

--- gg/main.swift
+++ gg/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+func findFactOf(_ num: Int) -> Int {
+	if num == 0 { return 1 }
+	
+	return (1...num).reduce(1, *)
+}
 
+var valuePull: Dictionary<Int, Character> {
+	var set = Dictionary<Int, Character>()
+	
+	(0...9).forEach { set[$0] = Character(String($0)) }
+	(0..<26).forEach { set[$0 + 10] = Character(UnicodeScalar("A".unicodeScalars.first!.value + UInt32($0))!) }
+	
+	return set
+}
+
+func dec2FactString(_ nb: Int) -> String {
+	var factValue = 1
+	var result = 1
+	while result * factValue < nb {
+		result *= factValue
+		factValue += 1
+	}
+	
+	var remained = nb
+	let array: [Character] = (0..<factValue).reversed().map {
+		let factorResult = findFactOf($0)
+		let multiplies = remained / factorResult
+		remained -= factorResult * multiplies
+		return valuePull[multiplies]!
+	}
+	
+	return String(array)
+}
+
+func factString2dDec(_ s: String) -> Int {
+	s.enumerated().map { index, char in
+		(s.count - index - 1, valuePull.first { $0.value == char}!.key)
+	}.compactMap { factorial, multiplies in
+		if multiplies > 0 {
+			return findFactOf(factorial) * multiplies
+		} else {
+			return nil
+		}
+	}.reduce(0, +)
+}
